85fe022400f30854e735476bd061e5ce14676b00
clouddeveloper.io <clouddeveloperio@users.noreply.github.com>
2017-07-23 23:29:45 -0400

    Refactored the application to use a new design as well as rebuilt the home page

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clouddeveloper.io';
+
+  ngOnInit(): void {
+    $(window).scroll(function(){
+        let wScroll = $(this).scrollTop();
+        $('.logo').css({
+          'transform': 'translate(0px, ' + wScroll * 1.53 + '%)'
+        });
+    });
+  }
 }
